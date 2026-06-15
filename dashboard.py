21527f503079b4d05e3175af4da848a9d976d7e2
Aknes <aknestasia2279@gmail.com>
2026-06-15 09:57:39 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -20,9 +20,9 @@ st.set_page_config(
 )
 
 # ==================================================
-# 2. AUTO REFRESH (Berjalan tiap 15 detik)
+# 2. AUTO REFRESH (Berjalan tiap 5 detik)
 # ==================================================
-st_autorefresh(interval=15000, key="refresh")
+st_autorefresh(interval=5000, key="refresh")
 
 # ==================================================
 # 3. GET DATA BACKEND
